c31e7bf0527d8dfc36b6078c65de4bd5d1741d5d
Gabriel Guzman <grg2117@columbia.edu>
2016-07-16 17:26:44 -0500

    various changes

--- SFS/SFS/urls.py
+++ SFS/SFS/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from SFS.views import hello, current_time, hours_ahead
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^hello/$', hello),
+    url(r'^time/$', current_time),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+
 ]
